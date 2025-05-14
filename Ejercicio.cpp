674327bb269453312617cfa85162408eb89eee3e
chris00193025 <00193025@uca.edu.sv>
2025-05-14 12:14:59 -0600

    Tercera parte completada :hammer:

--- Ejercicio.cpp
+++ Ejercicio.cpp
@@ -7,6 +7,15 @@ int main()
 
     int userage;
     int usercategory;
+    double wallet;
+    double walletTotal;
+    int userpurchase;
+    double productstudent;
+    double discountstudent;
+    double productpro;
+    double discountpro;
+
+    wallet = 1000;
 
     cout << "Antes debe de ingresar ingrese su edad" << endl;
     cin >> userage;
@@ -39,21 +48,129 @@ int main()
                 switch (usercategory)
                 {
                 case 1:
-                    
+                    cout << "Seleccione el producto que desea comprar: " << endl;
+                    cout << "1 | Laptop Basica: $900" << endl;
+                    cout << "2 | Tablet Estudiantil: $600" << endl;
+                    cout << "3 | Chromebook: $700" << endl;
+                    cin >> userpurchase;
+
+                    if (userpurchase > 3 || userpurchase < 1)
+                    {
+                        cout << "Ingrese un numero valido" << endl;
+                    }
+                    else
+                    {
+                        switch (userpurchase)
+                        {
+                        case 1:
+                            
+
+                            discountstudent = 900 * 0.20;
+                            productstudent = 900 - discountstudent;
+                            walletTotal = wallet - productstudent;
+
+                            cout << "Gracias por su compra el saldo total en su cuenta es: " << walletTotal << endl;
+
+                            break;
+                        case 2:
+
+                            discountstudent = 600 * 0.20;
+                            productstudent = 600 - discountstudent;
+                            walletTotal = wallet - productstudent;
+
+                            cout << "Gracias por su compra el saldo total en su cuenta es: " << walletTotal << endl;
+
+                            break;
+                        case 3:
+                            
+
+                            discountstudent = 700 * 0.20;
+                            productstudent = 700 - discountstudent;
+                            walletTotal = wallet - productstudent;
+
+                            cout << "Gracias por su compra el saldo total en su cuenta es: " << walletTotal << endl;
+
+                            break;
+                        default:
+                            break;
+                        }
+                    }
+
                     break;
                 case 2:
-                    /* code */
-                    break;
-                case 3:
-                    /* code */
-                    break;
+                    cout << "Seleccione el producto que desea comprar: " << endl;
+                    cout << "1 | Laptop Avanzada: $1500" << endl;
+                    cout << "2 | Tablet Pro: $1200" << endl;
+                    cout << "3 | Estacion de trabajo: $700" << endl;
+                    cin >> userpurchase;
 
-                default:
-                    break;
+                    if (userpurchase > 3 || userpurchase < 1)
+                    {
+                        cout << "Ingrese un numero valido" << endl;
+                    }
+                    else
+                    {
+                        switch (userpurchase)
+                        {
+                        case 1:
+                            
+
+                            discountpro = 1500 * 0.20;
+                            productpro = 1500 - discountpro;
+                            walletTotal = wallet - productpro;
+
+                            if (walletTotal < 1000)
+                            {
+                                cout << "No puede realizar la compra saldo insuficiente" << endl;
+                            }
+                            else
+                            {
+                                cout << "Gracias por su compra el saldo total en su cuenta es: " << walletTotal << endl;
+                            }
+
+                            break;
+                        case 2:
+                            
+
+                            discountpro = 1200 * 0.20;
+                            productpro = 1200 - discountpro;
+                            walletTotal = wallet - productpro;
+
+                            if (walletTotal < 1000)
+                            {
+                                cout << "No puede realizar la compra saldo insuficiente" << endl;
+                            }
+                            else
+                            {
+                                cout << "Gracias por su compra el saldo total en su cuenta es: " << walletTotal << endl;
+                            }
+
+                            break;
+                        case 3:
+                            
+
+                            discountpro = 700 * 0.20;
+                            productpro = 700 - discountpro;
+                            walletTotal = wallet - productpro;
+
+                            cout << "Gracias por su compra el saldo total en su cuenta es: " << walletTotal << endl;
+
+                            break;
+                        default:
+                            break;
+                        }
+                        break;
+                    case 3:
+                        /* code */
+                        break;
+
+                    default:
+                        break;
+                    }
                 }
             }
         }
-    }
 
-    return 0;
+        return 0;
+    }
 }
